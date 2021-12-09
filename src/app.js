c386e61e207aaf26d11c483885c8917a391ccfe7
felipecabeza16 <felipecabezas98@gmail.com>
2021-12-09 12:03:18 -0500

    elimiinar node modules

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const redis = require('redis');
 require('./db/index');
 const blogRouter = require('./routers/blog');
 
@@ -7,6 +8,13 @@ const app = express();
 app.use(express.json());
 
 app.use(blogRouter)
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+    console.log('conectado')
+});
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,4 +1,5 @@
 
+const redis = require('redis');
 const Blog = require('../models/blog');
 
 exports.getBlog = async (req, res) => {
@@ -16,9 +17,14 @@ exports.getBlog = async (req, res) => {
 exports.postBlog = async (req, res) => {
 
     try {
+        // conexion con mongo
         const blog = new Blog(req.body);
         await blog.save();
-        res.send({blog})
+        res.send({ blog })
+        // conexion con redis        
+        const client = redis.createClient();
+        client.set('blog', JSON.stringify(blog));
+
     } catch (error) {
         console.log(error);
         res.send(400).send(error);
