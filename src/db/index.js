62e434edb8926946e2aff4f8588af12664448ca8
GaVO <0vega.gabriel@gmail.com>
2021-12-10 16:18:24 -0500

    ":)"

--- src/db/index.js
+++ src/db/index.js
@@ -5,4 +5,4 @@ mongoose.connect('mongodb://localhost/blog-api-dev', {
     // useCreateIndex: true,
     // useFindAndModify: false,
     useUnifiedTopology: true
-}).then(db => console.log('DB connected ', db.connection.host))
+}).then(db => console.log('DB connected ', db.connection.host)).catch((e) => console.log("error: "+e));
